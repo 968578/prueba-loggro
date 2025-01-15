dd0a5901e1ecdb1fc82f6b467717549986267e26
968578 <omar.echavarria56@gmail.com>
2025-01-15 01:38:14 -0500

    docker-compose con api

--- front/src/services/handler-response.js
+++ front/src/services/handler-response.js
@@ -1,3 +1,23 @@
+/**
+ * @fecha 2025-01-12
+ * @autor Omar Echavarria
+ * @descripcion Esta función maneja la respuesta de una solicitud de API común. Dependiendo del mensaje en la 
+ * respuesta (`res.message`), la función realiza diferentes acciones:
+ * - Si el mensaje es "fail token", elimina el token de autenticación del `localStorage` y redirige al usuario 
+ *   a la página principal.
+ * - Si el mensaje es "server error", muestra una alerta notificando un error en el servidor.
+ * - Si el mensaje es "Unauthorized", ejecuta la función `cbUnauthorized` (si se proporciona) para manejar 
+ *   el caso de falta de autorización.
+ * - Si el mensaje es "ok", ejecuta la función `callback` pasada como parámetro para realizar la acción correspondiente.
+ * 
+ * @param {object} res - La respuesta de la API, que debe contener un mensaje (`message`) que indique el resultado 
+ *                       de la solicitud.
+ * @param {function} callback - Una función que se ejecuta si la respuesta es "ok".
+ * @param {function} cbUnauthorized - Una función que se ejecuta si la respuesta es "Unauthorized".
+ * 
+ * @returns {void} - No retorna ningún valor, solo maneja la respuesta de la API y ejecuta las funciones 
+ *                    correspondientes.
+ */
 export const handleCommon = (res, callback, cbUnauthorized ) => {
   if (res.message == "fail token") {
     window.localStorage.removeItem("token-prueba-loggro");
