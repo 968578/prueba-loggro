89725c396c013b007d04fd6ec43959f017edcafe
968578 <omar.echavarria56@gmail.com>
2025-01-15 03:05:48 -0500

    crear usuario despues en mongo

--- db/mongo-init.js
+++ db/mongo-init.js
@@ -1,13 +1,20 @@
+db.getSiblingDB('admin').createUser(
+    {
+        user: "root",
+        pwd: "root",
+        roles: ["root"]
+    })
+
 // Usuarios para autenticarse en la aplicación
 db.users.insertMany([
     {
         _id: 1,
-        name:"omar.echavarria",
+        name: "omar.echavarria",
         password: "$2a$11$R2oZz1hO/E2kEKiZs2c1tu4RP.pDgdajsw0dJdIxvO7kc4HZUfhDi"
     },
     {
         _id: 2,
-        name:"jhan.ibarra",
+        name: "jhan.ibarra",
         password: "$2a$11$R2oZz1hO/E2kEKiZs2c1tudtYOj6HHNftQP0F2Z03IRSK0/wzdcmu"
     },
 ]);
